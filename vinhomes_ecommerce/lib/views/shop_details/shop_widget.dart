85af8acb2ff570399e89f9a5395efe96229311bf
khoipham226 <liemkhoi@gmail.com>
2023-06-19 13:08:21 +0700

    Khoi/1308/190623 - Back button

--- vinhomes_ecommerce/lib/views/shop_details/shop_widget.dart
+++ vinhomes_ecommerce/lib/views/shop_details/shop_widget.dart
@@ -14,21 +14,36 @@ class ShopWidget extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
         child: Column(
           children: [
-            Padding(
-      padding: EdgeInsets.all(1),
-      child: SizedBox(
-            height: 100,
-            width: double.infinity,
-            child: Image.network(
-                'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
-                alignment: Alignment.bottomCenter,
-                fit: BoxFit.fitWidth)),
-              ), const Padding(
-              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+            Stack(
+              alignment: AlignmentDirectional.topStart,
+              children: [
+                SizedBox(
+                    height: 200,
+                    width: double.infinity,
+                    child: Image.network(
+                        'https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg',
+                        alignment: Alignment.center,
+                        fit: BoxFit.fitWidth)),
+                Positioned(
+                    top: 50,
+                    left: 20,
+                    child: ElevatedButton.icon(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const Icon(Icons.arrow_left_sharp),
+                      label: const Text('Back'),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                    )),
+              ],
+            ), const Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
               child: Text("Welcome to Lunch Shop"),
             ),
             Padding(
-                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
                 child: SeperatedList()),
           ],
         ),
